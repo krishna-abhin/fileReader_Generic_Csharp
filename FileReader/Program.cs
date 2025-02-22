ec0b42c4815fffd8d928aaba0e7605fcdd45c7a5
abhin <abhin@DESKTOP-2765N32>
2025-02-22 13:17:40 +0530

    Initial commit

---
+++ FileReader/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyConsole;
+
+Console.WriteLine("Hello, World!");
+List<Comma> comma = FileReader.Read<Comma>("Path\\Comma.csv", ",");
+List<PipeLine> pipeLine = FileReader.Read<PipeLine>("Path\\PipeLine.csv", "|");
+List<At> at = FileReader.Read<At>("Path\\New Text Document.txt", "@");
+
+
+string g = "";
